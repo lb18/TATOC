1ab7616d77e8d28d6d003d75433f27dcf7193090
lb18 <40322896+lb18@users.noreply.github.com>
2018-06-25 17:55:48 +0530

    Update TestTatoc.java

--- src/assignment/TestTatoc.java
+++ src/assignment/TestTatoc.java
@@ -1,6 +1,8 @@
 package assignment;
 import static org.testng.Assert.assertEquals;
-
+import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class TestTatoc extends Implementation
@@ -18,21 +20,27 @@ public class TestTatoc extends Implementation
 	public void test_Frame_Dungeon()
 	{
 		automate_Task_Frame_Dungeon();
-		assertEquals(driver.getCurrentUrl(), "http://10.0.1.86/tatoc/basic/drag");
+		WebElement get_id_name = driver.findElement(By.id("dropbox"));
+		assertEquals(get_id_name.getText(), "DROPBOX");
 	}
 	  
 	@Test (dependsOnMethods = {"test_Frame_Dungeon"})
 	public void test_Task_Drag_Around()
 	{
 		automate_Task_Drag_Around();
-		assertEquals(driver.getCurrentUrl(), "http://10.0.1.86/tatoc/basic/windows");
+		WebElement get_the_text_of_first_heading = driver.findElement(By.cssSelector("body > div > div.page > h1"));
+		System.out.println(get_the_text_of_first_heading.getText());
+		
+		// contains(substr) returns true if a string contains substring 'substr'
+		assertEquals(get_the_text_of_first_heading.getText().contains("Pop"), true);
 	}
 	  
 	@Test (dependsOnMethods = {"test_Task_Drag_Around"})
 	public void test_Task_Popup_Windows()
 	{
 		automate_Task_Popup_Windows();
-		assertEquals(driver.getCurrentUrl(), "http://10.0.1.86/tatoc/basic/cookie");
+		WebElement get_link_text = driver.findElement(By.linkText("Generate Token"));
+		Assert.assertTrue(get_link_text.getText().contains(" Tok"));
 	}
 	  
 	@Test (dependsOnMethods = {"test_Task_Popup_Windows"})
